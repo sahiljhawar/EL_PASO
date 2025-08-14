5348b0ac3be1ef682c7de7a5faf82894ee4a822f
Bernhard Haas <bhaas@spacepager3.gfz-potsdam.de>
2025-08-14 10:36:08 +0200

    Added slurm submit files.

---
+++ scripts/job_script_template.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=data_processing
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=4
+#SBATCH --output=output_%j.log
+#SBATCH --reservation=download
+
+# Get the start and end times from the command line arguments
+START_TIME=$1
+END_TIME=$2
+
+echo "Processing data from $START_TIME to $END_TIME"
+
+# Replace 'your_program.py' with the path to your data processing script.
+python ../examples/VanAllenProbes/process_efw_emfisis_density_combined.py "$START_TIME" "$END_TIME" "IRBEM/libirbem.so"
