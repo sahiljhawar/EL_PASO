6be60e532c17c4b186ef7bc93f2dec5097205564
Bernhard Haas <bhaas@spacepager3.gfz-potsdam.de>
2025-12-17 11:11:04 +0100

    Added REUSE information.

--- tests/unittests/test_magnetic_field_model.py
+++ tests/unittests/test_magnetic_field_model.py
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: 2025 GFZ Helmholtz Centre for Geosciences
+# SPDX-FileContributor: Bernhard Haas
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from datetime import datetime, timedelta, timezone
 from pathlib import Path
 from typing import Literal
