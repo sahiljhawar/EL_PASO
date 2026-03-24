c621bf6e6819db7ea070703a8bb77b8127ca29b4
Sahil Jhawar <jhawar@gfz.de>
2026-03-24 15:13:04 +0100

    rename proba-v to make it importable

--- examples/PROBAV/process_ept_electron_fluxes.py
+++ examples/PROBAV/process_ept_electron_fluxes.py
@@ -20,6 +20,7 @@ import el_paso as ep
 CHI2_BAD_QUALITY_THRESHOLD = 2
 EPT_ENERGY_LIMITS = [0.5, 0.6, 0.7, 0.8, 1.0, 2.4, 8.0]
 
+
 def process_ept_electron_fluxes(
     raw_data_path: str | Path,
     processed_data_path: str | Path,
@@ -29,8 +30,7 @@ def process_ept_electron_fluxes(
     num_cores: int = 32,
     bin_cadence: timedelta = timedelta(seconds=10),
 ) -> None:
-
-    load_dotenv("esa.env")
+    load_dotenv("/home/jhawar/EL_PASO_GH/esa.env")
 
     client_id = os.environ.get("CLIENT_ID")
     client_secret = os.environ.get("CLIENT_SECRET")
@@ -63,26 +63,24 @@ def process_ept_electron_fluxes(
     flux_unit = typing.cast("u.Unit", (u.cm**2 * u.s * u.sr * u.MeV) ** (-1))
 
     extraction_infos = [
-        ep.ExtractionInfo(result_key="year",name_or_column="Y", unit=u.dimensionless_unscaled, np_dtype=np.int32),
+        ep.ExtractionInfo(result_key="year", name_or_column="Y", unit=u.dimensionless_unscaled, np_dtype=np.int32),
         ep.ExtractionInfo(result_key="month", name_or_column="M", unit=u.dimensionless_unscaled, np_dtype=np.int32),
         ep.ExtractionInfo(result_key="day", name_or_column="D", unit=u.dimensionless_unscaled, np_dtype=np.int32),
         ep.ExtractionInfo(result_key="hour", name_or_column="H", unit=u.dimensionless_unscaled, np_dtype=np.int32),
         ep.ExtractionInfo(result_key="minute", name_or_column="MI", unit=u.dimensionless_unscaled, np_dtype=np.int32),
         ep.ExtractionInfo(result_key="second", name_or_column="S", unit=u.dimensionless_unscaled, np_dtype=np.int32),
-        ep.ExtractionInfo(result_key="millisecond", name_or_column="mS", unit=u.dimensionless_unscaled, np_dtype=np.int32),
-
-        ep.ExtractionInfo(result_key="flag",name_or_column="FLAG",unit=u.dimensionless_unscaled),
-        ep.ExtractionInfo(result_key="chi2",name_or_column="e-Chi2",unit=u.dimensionless_unscaled),
-
+        ep.ExtractionInfo(
+            result_key="millisecond", name_or_column="mS", unit=u.dimensionless_unscaled, np_dtype=np.int32
+        ),
+        ep.ExtractionInfo(result_key="flag", name_or_column="FLAG", unit=u.dimensionless_unscaled),
+        ep.ExtractionInfo(result_key="chi2", name_or_column="e-Chi2", unit=u.dimensionless_unscaled),
         ep.ExtractionInfo(result_key="ch0", name_or_column="e-fl-00", unit=flux_unit),
         ep.ExtractionInfo(result_key="ch1", name_or_column="e-fl-01", unit=flux_unit),
         ep.ExtractionInfo(result_key="ch2", name_or_column="e-fl-02", unit=flux_unit),
         ep.ExtractionInfo(result_key="ch3", name_or_column="e-fl-03", unit=flux_unit),
         ep.ExtractionInfo(result_key="ch4", name_or_column="e-fl-04", unit=flux_unit),
         ep.ExtractionInfo(result_key="ch5", name_or_column="e-fl-05", unit=flux_unit),
-
         ep.ExtractionInfo(result_key="PA_local", name_or_column="Pitch", unit=u.deg),
-
         ep.ExtractionInfo(result_key="rad", name_or_column="Rad", unit=u.km),
         ep.ExtractionInfo(result_key="lon", name_or_column="Long", unit=u.deg),
         ep.ExtractionInfo(result_key="lat", name_or_column="Lat", unit=u.deg),
@@ -99,8 +97,16 @@ def process_ept_electron_fluxes(
     )
 
     # create flux variable
-    flux_data = np.stack([variables["ch0"].get_data(), variables["ch1"].get_data(), variables["ch2"].get_data(),
-                          variables["ch3"].get_data(), variables["ch4"].get_data(), variables["ch5"].get_data()]).T
+    flux_data = np.stack(
+        [
+            variables["ch0"].get_data(),
+            variables["ch1"].get_data(),
+            variables["ch2"].get_data(),
+            variables["ch3"].get_data(),
+            variables["ch4"].get_data(),
+            variables["ch5"].get_data(),
+        ]
+    ).T
     flux_data = flux_data[:, :, np.newaxis]
     variables["FEDU"] = ep.Variable(data=flux_data, original_unit=flux_unit)
     del variables["ch0"], variables["ch1"], variables["ch2"], variables["ch3"], variables["ch4"], variables["ch5"]
@@ -108,16 +114,26 @@ def process_ept_electron_fluxes(
     # apply chi-2 quality check
     variables["FEDU"].apply_mask(variables["chi2"].get_data().astype(np.float64) < CHI2_BAD_QUALITY_THRESHOLD)
     variables["FEDU"].metadata.add_processing_note(
-        f"Values with CHI2 >= {CHI2_BAD_QUALITY_THRESHOLD:0.1f} are set to NaN.")
+        f"Values with CHI2 >= {CHI2_BAD_QUALITY_THRESHOLD:0.1f} are set to NaN."
+    )
 
     # expand PA variable
     variables["PA_local"].set_data(variables["PA_local"].get_data()[:, np.newaxis], unit="same")
 
     # create Epoch variable
-    epoch_datetime = [datetime(y, m, d, h, mi, s, int(ms), tzinfo=timezone.utc) for (y, m, d, h, mi, s, ms) in
-                      zip(variables["year"].get_data(), variables["month"].get_data(), variables["day"].get_data(),
-                          variables["hour"].get_data(), variables["minute"].get_data(), variables["second"].get_data(),
-                          variables["millisecond"].get_data().astype(np.int32)*1e3, strict=True)]
+    epoch_datetime = [
+        datetime(y, m, d, h, mi, s, int(ms), tzinfo=timezone.utc)
+        for (y, m, d, h, mi, s, ms) in zip(
+            variables["year"].get_data(),
+            variables["month"].get_data(),
+            variables["day"].get_data(),
+            variables["hour"].get_data(),
+            variables["minute"].get_data(),
+            variables["second"].get_data(),
+            variables["millisecond"].get_data().astype(np.int32) * 1e3,
+            strict=True,
+        )
+    ]
     epoch_data = [t.timestamp() for t in epoch_datetime]
 
     variables["Epoch"] = ep.Variable(data=np.asarray(epoch_data), original_unit=ep.units.posixtime)
@@ -128,7 +144,8 @@ def process_ept_electron_fluxes(
     energy_data = np.convolve(EPT_ENERGY_LIMITS, np.ones(2), "valid") / 2
     variables["Energy_FEDU"] = ep.Variable(data=energy_data, original_unit=u.MeV)
     variables["Energy_FEDU"].metadata.add_processing_note(
-        f"Created by calculating center energies from {', '.join(map(str, EPT_ENERGY_LIMITS))}.")
+        f"Created by calculating center energies from {', '.join(map(str, EPT_ENERGY_LIMITS))}."
+    )
 
     time_bin_methods = {
         "Energy_FEDU": ep.TimeBinMethod.Repeat,
@@ -144,7 +161,11 @@ def process_ept_electron_fluxes(
     )
 
     xsph_arr = np.stack(
-        (variables["rad"].get_data(ep.units.RE), variables["lat"].get_data(u.degree), variables["lon"].get_data(u.degree))
+        (
+            variables["rad"].get_data(ep.units.RE),
+            variables["lat"].get_data(u.degree),
+            variables["lon"].get_data(u.degree),
+        )
     ).T.astype(np.float64)
     model_coord = ep.processing.magnetic_field_utils.Coords(lib_path=irbem_lib_path)
 
@@ -200,7 +221,7 @@ def process_ept_electron_fluxes(
         mag_field="T89",
         data_standard=ep.data_standards.PRBEMStandard(),
     )
-    append = False
+    append = True
 
     ep.save(variables_to_save, saving_strategy, start_time, end_time, time_var=binned_time_var, append=append)
 
@@ -209,9 +230,7 @@ if __name__ == "__main__":
     logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
     logging.getLogger().setLevel(logging.INFO)
 
-    parser = argparse.ArgumentParser(
-        description="Process EPT electron fulx data."
-    )
+    parser = argparse.ArgumentParser(description="Process EPT electron fulx data.")
     parser.add_argument(
         "--start_time",
         type=str,
