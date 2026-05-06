1963f7c1c9471095ad1168f503043c29b87ef258
Sahil Jhawar <jhawar@gfz.de>
2026-05-06 14:38:41 +0200

    add contrib name

--- el_paso/saving_strategies/density_netcdf_strategy.py
+++ el_paso/saving_strategies/density_netcdf_strategy.py
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: 2025 GFZ Helmholtz Centre for Geosciences
 # SPDX-FileContributor: Bernhard Haas
+# SPDX-FileContributor: Sahil Jhawar
 #
 # SPDX-License-Identifier: Apache-2.0
 
