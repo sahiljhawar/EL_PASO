91f296aa11ad4bdfd7f53e5904feb4b2526df3d6
Sahil Jhawar <jhawar@gfz.de>
2026-05-21 17:15:20 +0200

    dont copy libirbem to CWD

--- setup.py
+++ setup.py
@@ -55,7 +55,6 @@ class CustomBuild(build_py):
         dest_so = os.path.join(dest_dir, "libirbem.so")
 
         cwd = os.getcwd()
-        dest_so_local = os.path.join(cwd, "libirbem.so")
         dest_so_el_paso_dir = os.path.join(cwd, "el_paso")
 
         tmp_dir = tempfile.mkdtemp(prefix="irbem_build_")
@@ -73,7 +72,6 @@ class CustomBuild(build_py):
                 )
 
             shutil.copy2(so_path, dest_so)
-            shutil.copy2(so_path, dest_so_local)
             shutil.copy2(so_path, dest_so_el_paso_dir)
 
         finally:
