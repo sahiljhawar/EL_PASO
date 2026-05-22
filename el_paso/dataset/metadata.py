9df43e25087b9f848917ae01bd09963404163c30
Sahil Jhawar <jhawar@gfz.de>
2026-05-22 19:47:53 +0200

    change year in header

--- el_paso/dataset/metadata.py
+++ el_paso/dataset/metadata.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2025 GFZ Helmholtz Centre for Geosciences
+# SPDX-FileCopyrightText: 2026 GFZ Helmholtz Centre for Geosciences
 # SPDX-FileContributor: Bernhard Haas
 # SPDX-FileContributor: Sahil Jhawar
 #
