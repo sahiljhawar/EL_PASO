98a8b3baa30b0489c6ddd11bc2bf0f9c1fa55658
Bernhard Haas <bhaas@gfz.de>
2026-06-02 07:25:12 +0200

    Added logger.py tests.

---
+++ tests/unittests/test_logger.py
@@ -0,0 +1,40 @@
+import logging
+from pathlib import Path
+
+import pytest
+
+import el_paso as ep
+
+logger = logging.getLogger(__name__)
+
+def test_setup_logging_write_to_file(tmp_path: Path):
+
+    log_file = tmp_path / "test.log"
+
+    ep.setup_logging(log_file=log_file)
+
+    logger.info("This is a test!")
+
+    with log_file.open("r") as f:
+        log_content = f.read()
+
+    assert "tests.unittests.test_logger:" in log_content
+    assert "This is a test!" in log_content
+
+def test_setup_logging_append_to_file(tmp_path: Path):
+
+    log_file = tmp_path / "test_append.log"
+    with log_file.open("w") as f:
+        f.write("HEADER\n")
+
+    ep.setup_logging(log_file=log_file, file_mode="a")
+
+    logger.info("This is a test!")
+
+    with log_file.open("r") as f:
+        line1 = f.readline()
+        assert line1 == "HEADER\n"
+
+        line2 = f.readline()
+        assert "tests.unittests.test_logger:" in line2
+        assert "This is a test!" in line2
