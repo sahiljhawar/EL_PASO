81d75beb52c99fc8b9c36e2f215644c42e5bdacb
Sahil Jhawar <jhawar@gfz.de>
2026-08-20 18:53:38 +0200

    import plot settings to init

--- el_paso/dataset/__init__.py
+++ el_paso/dataset/__init__.py
@@ -2,6 +2,12 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+from el_paso.dataset.bin_and_interpolate_to_model_grid import (
+    DebugPlotSettings,
+    bin_and_interpolate_to_model_grid,
+    plot_debug_figures,
+    plot_debug_figures_plasmasphere,
+)
 from el_paso.dataset.dataset import DataSet as DataSet
 from el_paso.dataset.dataset_implementations import GFZDataSet as GFZDataSet
 from el_paso.dataset.dataset_implementations import PRBEMDataSet as PRBEMDataSet
